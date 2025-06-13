af47e2b40b0d289d01a13100e1d0beb45e59aba3
Musiimenta Agnes <musiimentaagnes9@gmail.com>
2025-06-13 22:11:55 +0300

    musiimenta-app

--- src/App.js
+++ src/App.js
@@ -1,13 +1,19 @@
 import logo from './logo.svg';
 import './App.css';
 
+
 function App() {
   return (
 <div>
-<h1>I love coding with all my heart</h1>
+<h1 class="display-1">I love coding with all my heart</h1>
 <p>My name is Musiimenta Agnes</p>
+<img src="d:\Desktop\OneDrive\Imagens\0ce2c6ad308d08d809265e188effecdd_8.jpeg"></img>
 </div>
   )   
 }
 
 export default App;
+
+function MyFriends (){
+  return 
+}
