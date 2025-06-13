f00fd2db6f05c628c0727572fbb58889fdbe12d5
Musiimenta Agnes <musiimentaagnes9@gmail.com>
2025-06-13 21:17:08 +0300

    Musiimenta-react-app

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+<div>
+<h1>I love coding with all my heart</h1>
+<p>My name is Musiimenta Agnes</p>
+</div>
+  )   
+}
+
+export default App;
