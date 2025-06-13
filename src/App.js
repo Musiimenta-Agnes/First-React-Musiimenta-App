bb8a9aaa1914fafa731a7afc657bcccf79bc0253
Musiimenta Agnes <musiimentaagnes9@gmail.com>
2025-06-13 22:43:28 +0300

    musiimenta-app

--- src/App.js
+++ src/App.js
@@ -2,18 +2,32 @@ import logo from './logo.svg';
 import './App.css';
 
 
+
 function App() {
   return (
-<div>
-<h1 class="display-1">I love coding with all my heart</h1>
-<p>My name is Musiimenta Agnes</p>
-<img src="d:\Desktop\OneDrive\Imagens\0ce2c6ad308d08d809265e188effecdd_8.jpeg"></img>
-</div>
+    <><div>
+      <h1 class="display-1">I love coding with all my heart</h1>
+      <p>My name is Musiimenta Agnes</p>
+
+    </div><div>
+        <ul class="nav nav-bar">
+          <li class="nav-item">
+            <a href='' class="nav-link">About me</a>
+          </li>
+          <li class="nav-item">
+            <a href='' class="nav-link">Skills</a>
+          </li>
+          <li class="nav-item">
+            <a href='' class="nav-link">Services</a>
+          </li>
+          <li class="nav-item">
+            <a href='' class="nav-link">Contact me</a>
+          </li>
+        </ul>
+      </div></>
   )   
 }
 
 export default App;
 
-function MyFriends (){
-  return 
-}
+
